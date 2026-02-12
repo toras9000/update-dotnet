5d595d1005bcf070167cbeb412193b0d1e554629
toras9000 <toras9000@example.com>
2026-02-12 21:56:57 +0900

    update script

--- update-dotnet.sh
+++ update-dotnet.sh
@@ -4,10 +4,15 @@ if [ "$(whoami)" != "root" ]; then
     exit
 fi
 
+set -eu
+
 script_dir=$(cd $(dirname $0);pwd)
 
-rm -rf /usr/share/dotnet 2> /dev/null
-
+echo Download install script
 wget https://dot.net/v1/dotnet-install.sh -O "$script_dir/dotnet-install.sh"
 
+echo Delete existing dotnet
+rm -rf /usr/share/dotnet 2> /dev/null
+
+echo Install dotnet
 bash "$script_dir/dotnet-install.sh" --version latest --install-dir /usr/share/dotnet
